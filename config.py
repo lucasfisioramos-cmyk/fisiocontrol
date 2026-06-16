47654d609d49251879c4adb87d1b48e450f80d29
lucasfisioramos-cmyk <lucasfisio.ramos@gmail.com>
2026-06-16 23:38:34 +0000

    chore: switch default DB to MariaDB (mysql+pymysql) and add PyMySQL dependency

--- config.py
+++ config.py
@@ -2,8 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev_secret_key')
+    # Use DATABASE_URL env var when available. Default to MariaDB (MySQL) using PyMySQL.
+    # Example: export DATABASE_URL='mysql+pymysql://user:password@host:3306/fisiocontrol'
     SQLALCHEMY_DATABASE_URI = os.getenv(
         'DATABASE_URL',
-        'sqlite:///fisiocontrol.db'
+        'mysql+pymysql://root:password@localhost:3306/fisiocontrol'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
